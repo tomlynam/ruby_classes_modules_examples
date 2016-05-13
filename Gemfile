3ca7cae5e04b667e0b4562add744555f91596839
Tom Lynam <tom.s.lynam@gmail.com>
2016-05-13 10:56:25 -0600

    add some gems and .gitignore
    I also changed some code
    I also add pry

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+ruby '2.3.1'
+
+gem 'pry', '~> 0.10.3'
+gem 'babbler', '~> 2.0.0'
--- ruby_classes_modules_examples.rb
+++ ruby_classes_modules_examples.rb
@@ -1,12 +1,17 @@
+require 'pry'
+require 'babbler'
+
 puts 'YAY Friday!'
 
 def keep_coding
   puts 'Pop question: Are you going to keep coding over the weekend? (yes / no)'
   input = gets.strip.downcase
+
   if input == 'yes'
     puts 'Good answer!'
   else
-    puts 'You serious, bro? Do you even code?'
+    # puts 'You serious, bro? Do you even code?'
+    puts Babbler.babble
     keep_coding
   end
 end
